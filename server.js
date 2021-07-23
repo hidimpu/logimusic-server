c6ff63da880ff8cb8b5263763013cf5cd6bf567b
hidimpu <parthds450@gmail.com>
2021-07-23 16:23:36 +0530

    improved accuracy

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post("/song", async (req, res) => {
 
   await api.initalize();
   const result = await api.search(
-    `${song.title} ${song.artist},  'full audio', 'topic'  `
+    `${song.title} ${song.artist},  ' full audio '   `
   );
 
   const videoId = result.content[0].videoId;
